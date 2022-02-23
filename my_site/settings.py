e44dce1bc6afd7f6548c7ca5c125a7dfced8c491
Dimazey <mof@list.ru>
2022-02-23 14:09:31 +0300

    to heroku release caput english authors only makemigr

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '#0-nq_!rl0u5j%nf(49zz+mttf=1az-**w7j+k#cosde+7w2+r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,16 +79,6 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#    'NAME': 'db_library',
-#    'USER': 'dimazey',
-#    'PASSWORD': '1234',
-#   'HOST': '127.0.0.1',
-#    'PORT': '5432',
-#    }
-#}
 DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Password validation

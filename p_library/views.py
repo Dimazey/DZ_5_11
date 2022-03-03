787f92a429be08dd411cd3af9fc52fa7793d60de
Dimazey <mof@list.ru>
2022-03-03 10:05:01 +0300

    to heroku templates block

--- p_library/views.py
+++ p_library/views.py
@@ -34,38 +34,50 @@ def index(request):
     }
     return HttpResponse(template.render(biblio_data, request))
 
+def books(request):
+    template = loader.get_template('book_list.html')
+    books = Book.objects.all()
+    biblio_data = {
+        "title": "мою библиотеку",
+        "books": books,
+           }
+    return HttpResponse(template.render(biblio_data, request))
+
+
 def book_increment(request):
     if request.method == 'POST':
         book_id = request.POST['id']
         if not book_id:
-            return redirect('/index/')
+            return redirect('/books/')
         else:
             book = Book.objects.filter(id=book_id).first()
             if not book:
-                return redirect('/index/')
+                return redirect('/books/')
             book.copy_count += 1
             book.save()
-        return redirect('/index/')
+        return redirect('/books/')
     else:
-        return redirect('/index/')
+        return redirect('/books/')
+
 
 def book_decrement(request):
     if request.method == 'POST':
         book_id = request.POST['id']
         if not book_id:
-            return redirect('/index/')
+            return redirect('/books/')
         else:
             book = Book.objects.filter(id=book_id).first()
         if not book:
-            return redirect('/index/')
+            return redirect('/books/')
         if book.copy_count < 1:
             book.copy_count = 0
         else:
             book.copy_count -= 1
         book.save()
-        return redirect('/index/')
+        return redirect('/books/')
     else:
-        return redirect('/index/')
+        return redirect('/books/')
+
 
 def publishers(request):
     template = loader.get_template('publishers.html')
